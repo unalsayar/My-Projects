e3b2a515c5d77a45605ba60b8d61fcac9e8b4380
Ünal Sayar <unalsayar@gmail.com>
2021-10-06 21:12:32 +0200

    flask-03-handling-routes added

---
+++ python/hands-on/flask-03-handling-routes-and-if-for/flask-03-handling-routes/app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, redirect, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'This is home page for no path, <h1> Welcome Home</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1> This is home page for no path!</h1>'
+
+@app.route('/error')
+def error():
+    return '<h1> Either you run into an error are you are not authorized!</h1>'
+
+@app.route('/hello')
+def hello():
+    return f'<h1>Hello, World! </h1>'
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for('error'))
+
+@app.route('/<name>') #Burada template kiralamadan dinamik olarak çalıştırıyoruz.
+def greet(name):
+    greet_format = f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greeting Page</title>
+</head>
+<body>
+    <h1>Hello, { name }!</h1>
+    <h1>Welcome to my Greeting Page</h1>
+</body>
+</html>
+    """
+    return greet_format
+
+
+@app.route('/greet_admin')
+def greet_admin():
+    return redirect(url_for('greet', name = 'Master Admin!!!'))
+
+@app.route('/<name>') # Bura da template kiralayarak dinamik olarak çalıştırıyoruz.
+def greeting(name):
+    return render_template('greet.html', name_html = name )
+
+
+@app.route('/list10')
+def list10():
+    return render_template('list10.html')
+
+@app.route('/evens')
+def evens():
+    return render_template('evens.html')
+
+
+
+
+if __name__ == '__main__':
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
